6d3c2b1a8e33b172a9711c4d5c6b1e25349a6dff
xkHeitor <heitorcarvalhorod@gmail.com>
2022-02-15 21:42:05 -0300

    refactoriging of class cpf

---
+++ src/Cpf.ts
@@ -0,0 +1,61 @@
+export default class Cpf {
+ 
+	public value: string;
+	private factors = {
+		firstDigit: 10,
+		secondDigit: 11,
+	}
+
+	constructor(value: string) {
+		if (!this.validate(value)) throw new Error("CPF Inválido");
+		this.value = value;
+	}
+
+    public getValue(): string {
+		return this.value;
+	}
+
+	private validate(cpf: string): boolean {
+		if (!cpf) 
+            return false;
+		
+        cpf = this.removeMasks(cpf);
+		if (!this.isValidLength(cpf) || this.hasAllDigitsEqual(cpf)) 
+            return false;
+            
+		const digit1 = this.calculateDigit(cpf, this.factors.firstDigit);
+		const digit2 = this.calculateDigit(cpf, this.factors.secondDigit);
+		let checkDigit = this.extractCheckDigit(cpf);
+		const calculatedDigit = `${digit1}${digit2}`;
+        console.log(checkDigit, calculatedDigit)
+		return checkDigit == calculatedDigit;
+	}
+
+	private removeMasks(cpf: string): string {
+		return cpf.replace(/\D/gim, "");
+	}
+
+	private isValidLength(cpf: string): boolean {
+		return cpf.length === 11;
+	}
+
+	private hasAllDigitsEqual (cpf: string): boolean {
+		const [firstDigit] = cpf;
+		return [...cpf].every(digit => digit === firstDigit);
+	}
+
+	private calculateDigit (cpf: string, factor: number): number {
+        let result = 0;
+		for (const digit of cpf) {
+            if (factor > 1) result += parseInt(digit) * factor--;
+		}
+        const divider = 11;
+        const remainingOfDivision = Math.trunc(result % divider);
+		return remainingOfDivision < 2 ? 0 : Math.abs(divider - remainingOfDivision);
+	}
+
+	private extractCheckDigit (cpf: string): string {
+		return cpf.slice(-2);
+	} 
+
+}
---
+++ test/Cpf.test.ts
@@ -0,0 +1,24 @@
+import Cpf from "../src/Cpf";
+
+describe('CPF', () => {
+
+    const cpfs = {
+        sameDigits: '111.111.111-11',
+        valid: '111.444.777-35',
+        invalid: '111.444.777-18',
+    };
+
+    it('Should be valid the CPF', () => {
+        const cpf = new Cpf(cpfs.valid);
+        expect(cpf.getValue()).toBe(cpfs.valid);
+    });
+
+    it('Should be invalid the CPF', () => {
+        expect(() => new Cpf(cpfs.invalid)).toThrow(new Error("CPF Inválido"));
+    });
+
+    it('Should be invalid the CPF with same digits', () => {
+        expect(() => new Cpf(cpfs.sameDigits)).toThrow(new Error("CPF Inválido"));
+    });
+
+});
